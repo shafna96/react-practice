4f2eaf7e80c4ded414cefc11e87d18e554a88d97
shafna96 <sfnmail2@gmail.com>
2023-03-01 17:12:42 +0530

    sidebar modified

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,25 +1,82 @@
 import React, { useState } from "react";
 import Content from "./Content";
 import ToggleButton from "./ToggleButton";
+import { Drawer, Button } from "react-daisyui";
 
 function SideBar() {
-  const [checkbox, setcheckBox] = useState(true);
+  const [checkbox, setcheckBox] = useState(false);
   const handleCheckbox = (e) => {
     setcheckBox((prevState) => !prevState);
   };
+
   return (
+    // <div>
+    //   {checkbox ? (
+    //     <div className="drawer drawer-mobile">
+    //       <input
+    //         id="my-drawer"
+    //         type="checkbox"
+    //         className="drawer-toggle"
+    //         onClick={handleCheckbox}
+    //       />
+    //       <div className="drawer-content bg-purple-50">
+    //         <Content />
+    //       </div>
+    //       <div className="drawer-side">
+    //         <label
+    //           htmlFor="my-drawer"
+    //           className="drawer-overlay  bg-base-200 justify-center items-center"
+    //         ></label>
+    //         <ul className="menu p-4 w-48 bg-base-200 text-base-content">
+    //           <li>
+    //             <a>Sidebar Item 1</a>
+    //           </li>
+    //           <li>
+    //             <a>Sidebar Item 2</a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   ) : (
+    //     <div className="drawer">
+    //       <input
+    //         id="my-drawer"
+    //         type="checkbox"
+    //         className="drawer-toggle"
+    //         onClick={handleCheckbox}
+    //       />
+    //       <div className="drawer-content bg-purple-50">
+    //         <Content />
+    //       </div>
+    //       {/* <div className="drawer-side">
+    //         <label
+    //           htmlFor="my-drawer"
+    //           className="drawer-overlay  bg-base-100 justify-center items-center"
+    //         ></label>
+    //         <ul className="menu p-4 w-48 bg-base-200 text-base-content">
+    //           <li>
+    //             <a>Sidebar Item 1</a>
+    //           </li>
+    //           <li>
+    //             <a>Sidebar Item 2</a>
+    //           </li>
+    //         </ul>
+    //       </div> */}
+    //     </div>
+    //   )}
+    // </div>
     <div>
-      {checkbox ? (
-        <div className="drawer drawer-mobile">
-          <input
-            id="my-drawer"
-            type="checkbox"
-            className="drawer-toggle"
-            onClick={handleCheckbox}
-          />
-          <div className="drawer-content bg-purple-50">
-            <Content />
-          </div>
+      <div className="drawer drawer-mobile">
+        <input
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+          onClick={handleCheckbox}
+        />
+        <div className="drawer-content bg-purple-50">
+          <Content />
+        </div>
+        {checkbox && (
           <div className="drawer-side">
             <label
               htmlFor="my-drawer"
@@ -34,34 +91,8 @@ function SideBar() {
               </li>
             </ul>
           </div>
-        </div>
-      ) : (
-        <div className="drawer">
-          <input
-            id="my-drawer"
-            type="checkbox"
-            className="drawer-toggle"
-            onClick={handleCheckbox}
-          />
-          <div className="drawer-content bg-purple-50">
-            <Content />
-          </div>
-          {/* <div className="drawer-side">
-            <label
-              htmlFor="my-drawer"
-              className="drawer-overlay  bg-base-100 justify-center items-center"
-            ></label>
-            <ul className="menu p-4 w-48 bg-base-200 text-base-content">
-              <li>
-                <a>Sidebar Item 1</a>
-              </li>
-              <li>
-                <a>Sidebar Item 2</a>
-              </li>
-            </ul>
-          </div> */}
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
